512c04496c7e0a35864808e484d7d535fcb4bbe1
Elias Huhsovitz <elias.tu512@gmail.com>
2026-03-11 00:02:50 +0100

    C++ 2025/1 part 2

--- cpp/2025/1/main.cpp
+++ cpp/2025/1/main.cpp
@@ -3,6 +3,10 @@
 #include <iostream>
 #include <fstream> 
 
+#define MAX_ROTATION 100
+#define MIN_ROTATION 0
+#define START_ROTATION 50
+
 using namespace std;
 
 int python_style_mod(int x, int m)
@@ -10,25 +14,59 @@ int python_style_mod(int x, int m)
     return ((x % m) + m) % m;
 }
 
-int f(int curr, char command, int val)
+int update_dial(int curr, char command, int val)
 {
-    int mult = (command == 'L') ? -1 : 1;
-    return python_style_mod((curr + val * mult), 100);
+    int direction = (command == 'L') ? -1 : 1;
+    return python_style_mod((curr + val * direction), MAX_ROTATION);
 }
 
 int solve_part_1(char* filename)
 {
     int count = 0;
-    int curr = 50;
-    string line;
+    int curr = START_ROTATION;
+
     ifstream file(filename);
 
-    while (getline(file, line)) 
+    for (std::string line; std::getline(file, line);) 
     {
         char command = line[0];
         int val = stoi(line.substr(1));
-        curr = f(curr, command, val);
+        curr = update_dial(curr, command, val);
         if (curr == 0) count++;
+    }
+
+    return count;
+}
+
+int count_zero_hits(int curr, int val, int direction)
+{
+    int distance_to_zero = (direction == 1) ? (MAX_ROTATION - curr) : curr;
+
+    if (distance_to_zero == 0) distance_to_zero = MAX_ROTATION;
+
+    if (val < distance_to_zero) return 0;
+
+    return 1 + (val - distance_to_zero) / MAX_ROTATION;
+}
+
+int solve_part_2(char* filename)
+{
+    int count = 0;
+    int curr = START_ROTATION;
+
+    ifstream file(filename);
+
+    for (string line; getline(file, line);) 
+    {
+        char command = line[0];
+        int val = stoi(line.substr(1));
+        
+        int direction = (command == 'L') ? -1 : 1;
+
+        count += count_zero_hits(curr, val, direction);
+
+        curr = python_style_mod(curr + val * direction, MAX_ROTATION);
+        //cout << "intermediate: " << intermediate << " curr: " << curr << " count: " << count << endl;
     }
 
     return count;
@@ -37,7 +75,10 @@ int solve_part_1(char* filename)
 
 int main(int argc, char **argv)
 {
-    int solution1 = solve_part_1(argv[1]);
-    cout << solution1;
+    char* filename = argv[1];
+    //int solution1 = solve_part_1(filename);
+    //cout << solution1 << endl;
+    int solution2 = solve_part_2(filename);
+    cout << solution2 << endl;
     return 0;
 }
