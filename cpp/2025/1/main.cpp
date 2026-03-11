e56738acb8518e9de40145a54451fc7afc6cc272
Elias Huhsovitz <elias.tu512@gmail.com>
2026-03-11 20:18:24 +0100

    c++ 2025/1 part 1

--- cpp/2025/1/main.cpp
+++ cpp/2025/1/main.cpp
@@ -1,7 +1,6 @@
-#include <cstdio>
-#include <string>
+#include <fstream>
 #include <iostream>
-#include <fstream> 
+#include <string>
 
 #define MAX_ROTATION 100
 #define MIN_ROTATION 0
@@ -9,76 +8,64 @@
 
 using namespace std;
 
-int python_style_mod(int x, int m)
-{
-    return ((x % m) + m) % m;
+int python_style_mod(int x, int m) { return ((x % m) + m) % m; }
+
+int update_dial(int curr, char command, int val) {
+	int direction = (command == 'L') ? -1 : 1;
+	return python_style_mod((curr + val * direction), MAX_ROTATION);
 }
 
-int update_dial(int curr, char command, int val)
-{
-    int direction = (command == 'L') ? -1 : 1;
-    return python_style_mod((curr + val * direction), MAX_ROTATION);
+int solve_part_1(char *filename) {
+	int count = 0;
+	int curr = START_ROTATION;
+
+	ifstream file(filename);
+
+	for (std::string line; std::getline(file, line);) {
+		char command = line[0];
+		int val = stoi(line.substr(1));
+		curr = update_dial(curr, command, val);
+		if (curr == 0) count++;
+	}
+
+	return count;
 }
 
-int solve_part_1(char* filename)
-{
-    int count = 0;
-    int curr = START_ROTATION;
+int count_zero_hits(int curr, int val, int direction) {
+	int distance_to_zero = (direction == 1) ? (MAX_ROTATION - curr) : curr;
 
-    ifstream file(filename);
+	if (distance_to_zero == 0) distance_to_zero = MAX_ROTATION;
 
-    for (std::string line; std::getline(file, line);) 
-    {
-        char command = line[0];
-        int val = stoi(line.substr(1));
-        curr = update_dial(curr, command, val);
-        if (curr == 0) count++;
-    }
+	if (val < distance_to_zero)
+		return 0;
 
-    return count;
+	return 1 + (val - distance_to_zero) / MAX_ROTATION;
 }
 
-int count_zero_hits(int curr, int val, int direction)
-{
-    int distance_to_zero = (direction == 1) ? (MAX_ROTATION - curr) : curr;
+int solve_part_2(char *filename) {
+	int count = 0;
+	int curr = START_ROTATION;
 
-    if (distance_to_zero == 0) distance_to_zero = MAX_ROTATION;
+	ifstream file(filename);
 
-    if (val < distance_to_zero) return 0;
+	for (string line; getline(file, line);) {
+		char command = line[0];
+		int val = stoi(line.substr(1));
 
-    return 1 + (val - distance_to_zero) / MAX_ROTATION;
+		int direction = (command == 'L') ? -1 : 1;
+
+		count += count_zero_hits(curr, val, direction);
+		curr = python_style_mod(curr + val * direction, MAX_ROTATION);
+	}
+
+	return count;
 }
 
-int solve_part_2(char* filename)
-{
-    int count = 0;
-    int curr = START_ROTATION;
-
-    ifstream file(filename);
-
-    for (string line; getline(file, line);) 
-    {
-        char command = line[0];
-        int val = stoi(line.substr(1));
-        
-        int direction = (command == 'L') ? -1 : 1;
-
-        count += count_zero_hits(curr, val, direction);
-
-        curr = python_style_mod(curr + val * direction, MAX_ROTATION);
-        //cout << "intermediate: " << intermediate << " curr: " << curr << " count: " << count << endl;
-    }
-
-    return count;
-}
-
-
-int main(int argc, char **argv)
-{
-    char* filename = argv[1];
-    //int solution1 = solve_part_1(filename);
-    //cout << solution1 << endl;
-    int solution2 = solve_part_2(filename);
-    cout << solution2 << endl;
-    return 0;
+int main(int argc, char **argv) {
+	char *filename = argv[1];
+	int solution1 = solve_part_1(filename);
+	cout << "Part 1: " << solution1 << "\n";
+	int solution2 = solve_part_2(filename);
+	cout << "Part 2: " << solution2 << endl;
+	return 0;
 }
